cf47cc10fc0b508151223e60562bbaa35670c624
Lindsay Taylor <llindsayttaylor@gmail.com>
2022-11-09 10:04:49 -0500

    finished core deliverables. attempting advanced deliverables

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,50 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([]);
+
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then (res => res.json())
+    .then ((plantData) => {
+      setPlants(plantData)
+    })
+  }, [])
+
+  function addNewPlant (formData){
+    const newPlantArray = [...plants, formData];
+    setPlants(newPlantArray);
+
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      body: JSON.stringify(formData),
+      headers: {
+        "Content-Type": "application/json"
+      }})
+  }
+  
+
+  function searchPlants (result){
+    const searchPlantsArray = plants.filter((plants)=> {
+      return plants.name.toLowerCase().includes(result.toLowerCase());
+    })
+    setPlants(searchPlantsArray);
+  }
+
+
+
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addNewPlant={addNewPlant}/>
+      <Search searchPlants={searchPlants}/>
+      <PlantList plants={plants} />
     </main>
   );
 }
